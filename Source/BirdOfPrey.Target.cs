11de5a8c66bc62778afdb960ed6b5b230b3a6ba9
EfeCanoglu <82241128+PitoYaga@users.noreply.github.com>
2023-11-02 18:47:46 +0300

    rar file implamented

--- Source/BirdOfPrey.Target.cs
+++ Source/BirdOfPrey.Target.cs
@@ -9,6 +9,7 @@ public class BirdOfPreyTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 
 		ExtraModuleNames.AddRange( new string[] { "BirdOfPrey" } );
 	}
--- Source/BirdOfPreyEditor.Target.cs
+++ Source/BirdOfPreyEditor.Target.cs
@@ -8,8 +8,9 @@ public class BirdOfPreyEditorTarget : TargetRules
 	public BirdOfPreyEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+        DefaultBuildSettings = BuildSettingsVersion.V4;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 
-		ExtraModuleNames.AddRange( new string[] { "BirdOfPrey" } );
+        ExtraModuleNames.AddRange( new string[] { "BirdOfPrey" } );
 	}
 }
